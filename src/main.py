64a18595f17fcf368d7c8ec50de1ae32af84512b
dlg1206 <dlg1206@g.rit.edu>
2023-06-01 22:43:40 -0400

    Streamline loading data

--- src/main.py
+++ src/main.py
@@ -10,40 +10,56 @@ from QuoteBot import start_bot
 from Util.Logger import init_log
 
 
-def main(src_dir):
-    # load file
+def load_quote_data(quotes_path):
+
+    # Attempt to read data
     try:
-        with open(src_dir + "\quotes.json") as json_file:
+        with open(quotes_path, "w") as json_file:
             quotes = json.load(json_file)
-    except:
-        print(f"Directory \'{src_dir}\' was not found")
+
+            # Make sure quote count correct
+            num_quotes = 0
+            for quotee in quotes['quotes']:
+                num_quotes += len(quotes['quotes'][quotee])
+
+            # Quotes are correct
+            if num_quotes == quotes['num_quotes']:
+                print("count and stored match")
+            # Update and fix count
+            else:
+                print("count and stored mismatched, updating ...")
+                quotes['num_quotes'] = num_quotes
+                json_file.write(json.dumps(quotes, indent=4))
+
+    except FileNotFoundError:
+        print(f"File \'{quotes_path}\' was not found")
         exit()
-    # Make sure quote count correct
-    num_quotes = 0
-    for quotee in quotes['quotes']:
-        num_quotes += len(quotes['quotes'][quotee])
+    except:
+        print(f"Failed to parse file \'{quotes_path}\'")
+        exit()
 
-    if num_quotes == quotes['num_quotes']:
-        print("count and stored match")
+    return quotes
+
+
+def main(quotes_path=None):
+
+    # Attempt to load data if path given
+    if quotes_path is not None:
+        quotes = load_quote_data(quotes_path)
     else:
-        print("count and stored mismatched, updating ...")
-        quotes['num_quotes'] = num_quotes
-        with open(src_dir + "/quotes.json", "w") as json_file:
-            json_file.write(json.dumps(quotes, indent=4))
+        quotes = {"num_quotes": 0, "quotes": {}}    # default
 
-    init_log(src_dir)
+    # init_log(quotes_path)
     print("Data loaded, Bot is starting . . .")
-    # Run Bot
-    start_bot(quotes, src_dir)
 
+    # Run Bot
+    start_bot(quotes, quotes_path)
     print("done")
 
 
 if __name__ == '__main__':
-    # Default to 'DemoQuotes' if directory is not given
+
     if len(sys.argv) != 1:
         main(sys.argv[1])
     else:
-        main("quotes")
-
-
+        main()
